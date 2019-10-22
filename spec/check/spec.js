27b8d025729db81f5484ba5e5eb7b3c6835c8b22
Rom4ig <43033763+Rom4ig@users.noreply.github.com>
2019-10-22 15:01:31 +0300

    Update spec.js

--- spec/check/spec.js
+++ spec/check/spec.js
@@ -7,6 +7,7 @@ let driver = new Builder().forBrowser('chrome').build();
 let count = 0;
 let start;
 
+
 beforeAll(function (done) {
   driver.get('https://google.com/').then(() => {
     driver.findElement(By.className("gLFyf gsfi")).then(tag => {
@@ -27,25 +28,22 @@ afterAll(async function () {
 }, 15000);
 
 describe('Google', function () {
-
-  it('FP', function (done) {
-    driver.getPageSource().then(source => {
-      expect(source.toLowerCase().includes(config.search.toLowerCase())).toBe(true);
-      done();
-    });
+  it('FP', async function (done) { //check First Page
+    console.log('Check1');
+    expect(await search()).toBe(true);
+    done();
   }, 10000);
 
-  it('SP', async function (done) {
-    await driver.wait(until.elementLocated(By.id('pnnext')));
+  it('SP', async function (done) { //check Second Page
+    await driver.wait(until.elementLocated(By.id('pnnext'))); //Go Second Page
     await driver.findElement(By.id('pnnext')).then(element =>
       element.click());
-    driver.getPageSource().then(source => {
-      expect(source.toLowerCase().includes(config.search.toLowerCase())).toBe(true);
-      done();
-    });
-  }, 10000);
+    console.log('Check2');
+    expect(await search()).toBe(true);
+    done();
+  }, 15000);
 
-  it('Results count greater than x', async function (done) {
+  it('Results count greater than x', async function (done) { //Check count of results
     let element = await driver.wait(until.elementLocated(By.id('resultStats')));
     let found = await element.getText();
     let regex = /(([0-9]+\s*)+)/;
@@ -57,3 +55,18 @@ describe('Google', function () {
   }, 10000);
 
 });
+async function search() { //Search for source text in headers
+  let count_h3 = 0;
+  let count_search = 0;
+  let check = false;
+  let i = await driver.findElements(By.css("h3.LC20lb")); //Search all headers
+  for (let link of i) { //Check headers
+    count_h3++;
+    let text = await link.getText();
+    if (text.toLowerCase().indexOf(config.search.toLowerCase()) !== -1) { count_search++; }
+    console.log(text);
+  }
+  console.log(`=============> ${count_search} and =========> ${count_h3}`)
+  if (count_h3 === count_search) { check = true };
+  return check;
+}
