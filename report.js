a5e362949d9a194d39024c44865ca153c1847b6f
Rom4ig <43033763+Rom4ig@users.noreply.github.com>
2019-11-03 23:40:45 +0300

    Delete report.js

--- report.js
+++
@@ -1,8 +0,0 @@
-let allure = require('allure-commandline');
-let generation = allure(['generate', 'allure-results', '--clean']);
-let open = require('open');
-
-generation.on('exit', function(exitCode) {
-    console.log(`Generation finished with code: ${exitCode}`);
-    open('allure-report/index.html');
-});
