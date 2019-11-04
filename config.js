684a3797488632c2fe55f78c8e4af3819d15cf86
Rom4ig <43033763+Rom4ig@users.noreply.github.com>
2019-11-04 10:40:16 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -23,7 +23,12 @@ exports.logger = {
   error: str => loggerDefault.error(str),
   fatal: str => loggerDefault.fatal(str)
 }
-const AllureReporter = require('jasmine-allure-reporter');
-jasmine.getEnv().addReporter(new AllureReporter({
-  resultsDir: 'allure-results'
-}));
+exports.config = {
+  framework: 'jasmine2',
+  onPrepare: function() {
+    var AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter({
+      resultsDir: 'allure-results'
+    }));
+  }
+}
