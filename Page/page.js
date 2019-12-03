1fb98bcb036d7a94d36ed39aa7fe35b1118ced91
Rom4ig <43033763+Rom4ig@users.noreply.github.com>
2019-12-03 21:55:38 +0300

    Add files via upload

---
+++ Page/page.js
@@ -0,0 +1,52 @@
+const config = require('../config');
+const logger = config.logger;
+
+class Page {
+
+    async click(selector, count) {
+        let element = await $(selector);
+        if (count === undefined) {
+            logger.trace('Click: ' + selector);
+            await element.click();
+        }
+        else {
+            logger.trace(`Click: ${selector} ${count} times`);
+            for (let i = 0; i < count; i++)
+                await element.click();
+        }
+    }
+
+    async back() {
+        logger.trace('Back page');
+        driver.back();
+    }
+
+
+    async setCreeds(selector, length) {
+        logger.trace(`Random creeds ${selector}`);
+        let creed = await $(`android=new UiSelector().resourceId("${selector}")`);
+        await creed.setValue(this.getRandom(length));
+    }
+
+    async submit() {
+        logger.trace(`Submit`);
+        let element = await $(`android=new UiSelector().resourceId("submit")`);
+        await element.click();
+    }
+
+    async getError(selector) {
+        logger.trace('Error');
+        let err = await $(selector);
+        return await err.getText();
+    }
+
+    getRandom(i) {
+        var rnd = '';
+        while (rnd.length < i)
+            rnd += Math.random().toString(36).substring(2);
+        return rnd.substring(0, i);
+    }
+
+}
+
+module.exports = new Page();
---
+++ test/specs/check/spec.js
@@ -0,0 +1,25 @@
+const config = require('../../../config');
+const logger = config.logger;
+const Page = require('../../../Page/page')
+const assert = require('assert').strict;
+
+describe('Android', function () {
+  it('Headers', async () => {
+    logger.info('Start describe');
+    //android = new UiSelector().description(“Go To Seller Filter”).childSelector(textContains(“Seller”))
+
+    //const log = 'new UiSelector().className("android.view.ViewGroup").index(1)'
+    //const login = $(`android=${log}`)
+    await Page.click(config.logo, 5);
+    await Page.click(config.QA1Button);
+    await Page.back();
+    await Page.click(config.loginButton);
+    await Page.setCreeds(config.name, 11);
+    await Page.setCreeds(config.pass, 13);
+    await Page.submit();
+    const err = await Page.getError(config.error);
+    expect(err).toContain(config.errors);
+    logger.info('End describe');
+  });
+
+});
