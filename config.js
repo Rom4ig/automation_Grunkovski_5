7d8c995e1d984950debc1c21d413834a7b73f82b
Rom4ig <43033763+Rom4ig@users.noreply.github.com>
2019-12-03 22:07:29 +0300

    Update config.js

--- config.js
+++ config.js
@@ -29,3 +29,4 @@ exports.name = 'user_username';
 exports.pass = 'user_password';
 exports.error = 'android=new UiSelector().className("android.view.View").textContains("error")';
 exports.errors = "We didn't recognize the username or password you entered.";
+exports.submit = `android=new UiSelector().resourceId("submit")`
